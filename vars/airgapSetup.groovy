3209831d497dc2002465f3cf1561b08c36d324e5
Daniel Newman <dan@danlovesprogramming.com>
2025-11-17 22:05:19 -0600

    Implement AirgapDestroyPipeline and update facade in airgapDestroy.groovy

--- vars/airgapSetup.groovy
+++ vars/airgapSetup.groovy
@@ -1,5 +1,6 @@
 import org.rancher.qa.airgap.AirgapSetupPipeline
 
+// Thin facade: expose setup pipeline constructor while implementation lives in src/org/rancher/qa/airgap/AirgapSetupPipeline.groovy
 def pipeline(def steps = this) {
     new AirgapSetupPipeline(steps)
 }
